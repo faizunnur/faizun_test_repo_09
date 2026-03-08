ab0286e180b7294f34e95c17463334522489efcf
faizunnur <faizunnur71@gmail.com>
2026-03-09 03:37:18 +0600

    Auto-fix attempt 3: terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,26 +1,26 @@
 output "instance_id" {
   description = "The ID of the EC2 instance running nginx"
-  value       = aws_instance.nginx.id
+  value       = aws_instance.server.id
 }
 
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.nginx.public_ip
+  value       = aws_instance.server.public_ip
 }
 
 output "instance_public_dns" {
   description = "The public DNS name of the EC2 instance"
-  value       = aws_instance.nginx.public_dns
+  value       = aws_instance.server.public_dns
 }
 
 output "nginx_url" {
   description = "The URL to access nginx"
-  value       = "http://${aws_instance.nginx.public_ip}"
+  value       = "http://${aws_instance.server.public_ip}"
 }
 
 output "security_group_id" {
   description = "The ID of the security group attached to the EC2 instance"
-  value       = aws_security_group.nginx_sg.id
+  value       = aws_security_group.sg.id
 }
 
 output "project_name" {
@@ -30,5 +30,5 @@ output "project_name" {
 
 output "ssh_connection_string" {
   description = "SSH connection string for the EC2 instance"
-  value       = "ssh -i your-key.pem ec2-user@${aws_instance.nginx.public_ip}"
+  value       = "ssh -i your-key.pem ec2-user@${aws_instance.server.public_ip}"
 }
